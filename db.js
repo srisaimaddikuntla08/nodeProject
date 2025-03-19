2258743b94c07aea634460dc47416a7061cf58e9
srisai810 <srisaimaddikuntlams@gmail.com>
2025-03-19 13:48:49 +0530

    shhh

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const mongoose =  require('mongoose')
 require("dotenv").config();
 
+
+
 async function connectToMongoDB(uri){
     return  await mongoose.connect(uri)
 }
+// URL in .env file
 
 
 module.exports = {connectToMongoDB}
